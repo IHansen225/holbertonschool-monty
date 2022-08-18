43be402abcd02be89a6162383d90cecb53e736c4
Ivan Hansen <ihansen@pop-os.localdomain>
2022-08-18 16:49:12 -0300

    Functional test

--- monty_aux.c
+++ monty_aux.c
@@ -21,6 +21,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (!(*stack))
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	else
 		printf("%d\n", (*stack)->n);
