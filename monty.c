9d02a3920e169938c0841dca1487318c980e8df6
Ivan Hansen <ihansen@pop-os.localdomain>
2022-08-18 01:24:27 -0300

    Apparently functional prototype

---
+++ monty.c
@@ -0,0 +1,98 @@
+#include "monty.h"
+
+void free_dlistint(stack_t *head)
+{
+	stack_t *p_elem, *elem;
+
+	elem = head;
+	while (elem)
+	{
+		p_elem = elem;
+		elem = elem->next;
+		free(p_elem);
+	}
+}
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *newelement;
+	char *n_num;
+
+	newelement = malloc(sizeof(stack_t));
+	if (newelement == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	newelement->next = *stack;
+	newelement->prev = NULL;
+	n_num = strtok(NULL, " \n");
+	if (n_num != NULL)
+		newelement->n = atoi(n_num);
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (newelement->next != NULL)
+		newelement->next->prev = newelement;
+	*stack = newelement;
+}
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack, (void) line_number;
+}
+
+void (*get_op_func(char *s))(stack_t **, unsigned int)
+{
+	instruction_t ops[] = {
+		{"push", push}, {"pall", pall},
+		{"pint", pint}, {"swap", swap},
+		{"pop", pop}, {"add", add},
+		{"nop", nop}, {NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(ops[i].opcode, s) == 0)
+		{
+			printf("%s\n", ops[i].opcode);
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+int main(int ac, char **av)
+{
+	unsigned int ln = 1;
+    FILE *fptr;
+    char *line = NULL, *tok;
+    size_t len = 0;
+	stack_t **stack;
+
+	*stack = NULL;
+	if (ac != 2)
+	{
+		printf("USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+    if (!(fptr = fopen(av[1], "r")))
+	{
+		printf("Error: Can't open file %s\n", av[1]);
+        exit(EXIT_FAILURE);
+	}
+    while ((getline(&line, &len, fptr)) != -1) 
+	{
+		tok = strtok(line, " \n");
+		(*get_op_func(tok))(stack, ln);
+    }
+	free_dlistint(*stack);
+    fclose(fptr);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
---
+++ monty_aux.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *p = *stack;
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	for (; p != NULL; p = p->next)
+		printf("%d\n", p->n);
+}
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	printf("%d\n", (*stack)->n);
+}
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *p = *stack;
+	stack_t *d = *stack;
+
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	p = p->next, p->prev = NULL;
+	*stack = p, free(d);
+}
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *p = *stack;
+	stack_t *d = (*stack)->next;
+
+	if ((!(*stack)) || ((*stack)->next == NULL))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	d->prev = NULL, p->prev = d;
+	p->next = d->next, d->next = p;
+	*stack = d;	
+}
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+	stack_t *p = *stack, *newelement;
+	stack_t *d = (*stack)->next;
+
+	if ((!(*stack)) || ((*stack)->next == NULL))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (p->n)+(d->n);
+	pop(stack, line_number), pop(stack, line_number);
+	newelement = malloc(sizeof(stack_t));
+	if (newelement == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	newelement->next = *stack;
+	newelement->prev = NULL;
+	newelement->n = sum;
+	if (newelement->next != NULL)
+		newelement->next->prev = newelement;
+	*stack = newelement;
+}
