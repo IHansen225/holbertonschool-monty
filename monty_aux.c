ea06efdcda5df5839d733d27a248af41022b0e87
Ivan Hansen <ihansen@pop-os.localdomain>
2022-08-18 21:40:30 -0300

    Final fixes

--- monty_aux.c
+++ monty_aux.c
@@ -123,12 +123,12 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else if ((*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	p = *stack, d = (*stack)->next;
